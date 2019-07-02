e21d17b7a953e196b587a51a25efc69bcedbb2f4
Trdat Mkrtchyan <t.mkrtchyan@atol.ru>
2019-07-03 00:58:44 +0300

    Initial

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cron = require('node-cron')
+const PORT = process.env.PORT || 5000
+const TOKEN = process.env.TOKEN;
+const CHAT_IDS = (process.env.CHAT_IDS || '').split(',').map(Number)
+
+const TelegramBot = require('node-telegram-bot-api');
+
+const bot = new TelegramBot(TOKEN, {
+    polling: true
+});
+
+bot.on("polling_error", (err) => console.log(err));
+
+bot.onText(/\/kushot/, (msg) => {
+    const chatId = msg.chat.id;
+
+    if (CHAT_IDS.indexOf(chatId) >= 0) {
+        const rando = (Math.random() * 19 >> 0)
+        bot.sendPhoto(chatId, `./imgs/${rando}.jpg`);
+    } else {
+        bot.sendMessage(chatId, 'Попроси @zeffirsky включить меня');
+    }    
+});
+
+cron.schedule(process.env.CRON || '55 00 * * 1-5', () => {
+    CHAT_IDS.forEach(chatId => {
+        const rando = (Math.random() * 19 >> 0)
+        bot.sendPhoto(chatId, `./imgs/${rando}.jpg`);
+    })
+}, {
+    scheduled: true,
+    timezone: 'Europe/Moscow'
+})
+
+express().get('/', (req, res) => res.send('Alive')).listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+
